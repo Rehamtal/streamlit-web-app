33100e8f2d1d3fe91f4f200dd7c66d1fe7f763f9
Reham Altalhi <89342811+Rehamtal@users.noreply.github.com>
2025-01-13 17:36:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,24 @@
 import streamlit as st
 from time import sleep
-from navigation import make_sidebar
+#from navigation import make_sidebar
 
-make_sidebar()
+#make_sidebar()
 
-st.title("Welcome to Diamond Corp")
 
-st.write("Please log in to continue (username `test`, password `test`).")
+st.title("Ready to explore data with Cube!")
 
+st.write("Please log in to continue.")
+
+# Ask the user for their username and password
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
+# In a real scenario, you'd validate these credentials with a database or some authentication mechanism.
+# Here we are using placeholder checks just for demonstration.
+
 if st.button("Log in", type="primary"):
-    if username == "test" and password == "test":
+    # Placeholder for checking valid username and password
+    if username == "your_valid_username" and password == "your_valid_password":
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
         sleep(0.5)
